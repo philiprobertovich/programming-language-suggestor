4545da0139d3c8c14c2d258985229b523422c259
Phil Kendall <prkkendall@gmail.com>
2022-11-07 07:02:46 -0800

    create functioning probgram for language suggester, including README file.

---
+++ README.md
@@ -0,0 +1,49 @@
+# Programming Language Selector 
+
+### By Philip Kendall
+
+#### This application presents to the user a quiz and then determines a great starting programming language for them.
+
+## Technologies Used
+
+* HTML
+* CSS
+* JavaScript
+* Git
+
+## Description
+
+This webpage will present the user with a 5 question form. After the user submits their form, the JavaScript will interpret that information and then output a suggestion of a programming language that would be well suited for the user.
+
+## Setup/Installation Requirements
+
+* Fork over the the repository to your own Github account.
+* Clone your Github repo down to your local machine and into the directory you would like this project to be stored.
+* Navigate to the index.html file and open it in your web browser.
+
+## Known Bugs
+
+Output section does not hide after the reset button is clicked.
+
+## License
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The below copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Copyright (c) 2022 Philip Kendall
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,106 @@
-// Recieves user input from form
-function handleSelect (event) {
+// Receives user input from form and displays output
+function handleForm (event) {
   event.preventDefault();
+  const reset = document.getElementById("reset");
+  reset.removeAttribute("class");
+  reset.addEventListener("reset", function() {
+    document.getElementById("output").setAttribute("class", "hidden");
+  });
+
   const question1 = document.querySelector("input[name='bagel']:checked").value;
   const question2 = document.querySelector("input[name='difficulty']:checked").value;
   const question3 = document.querySelector("input[name='goals']:checked").value;
+
+  document.getElementById("output").removeAttribute("class");
+  document.querySelector("span").innerText = processQuestions(question1, question2, question3);
 }
+
+// Take the values of the questions, interpret them, and returns a language 
+function processQuestions(question1, question2, question3) {  
+  if (question1 == "plain" && question2 == "easy" && question3 == "webpage") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "easy" && question3 == "software") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "easy" && question3 == "problem-solving") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "moderate" && question3 == "webpage") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "moderate" && question3 == "software") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "moderate" && question3 == "problem-solving") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "hard" && question3 == "webpage") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "hard" && question3 == "software") {
+    return "Javascript";
+  }
+  if (question1 == "plain" && question2 == "hard" && question3 == "problem-solving") {
+    return "Javascript";
+  }
+  if (question1 == "everything" && question2 == "easy" && question3 == "webpage") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "easy" && question3 == "software") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "easy" && question3 == "problem-solving") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "moderate" && question3 == "webpage") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "moderate" && question3 == "software") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "moderate" && question3 == "problem-solving") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "hard" && question3 == "webpage") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "hard" && question3 == "software") {
+    return "C#";
+  }
+  if (question1 == "everything" && question2 == "hard" && question3 == "problem-solving") {
+    return "C#";
+  }
+  if (question1 == "raisin" && question2 == "easy" && question3 == "webpage") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "easy" && question3 == "-software") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "easy" && question3 == "problem-solving") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "moderate" && question3 == "webpage") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "moderate" && question3 == "software") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "moderate" && question3 == "problem-solving") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "hard" && question3 == "webpage") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "hard" && question3 == "software") {
+    return "Python";
+  }
+  if (question1 == "raisin" && question2 == "hard" && question3 == "problem-solving") {
+    return "Python";
+  }
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", handleForm);
+});
